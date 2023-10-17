66005221c748a509949017fb95ef5374fe47cb25
hoangquan2002 <20521799@gm.uit.edu.vn>
2023-10-17 10:15:35 +0700

    bigupdate

--- project/app/views.py
+++ project/app/views.py
@@ -13,10 +13,18 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
     else:
-        items=[]
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
     context={'items': items, 'order':order}
     return render(request,'app/cart.html',context)
 
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+    context={'items': items, 'order':order}
     return render(request,'app/checkout.html',context)
